2c19d7093e4a31ffaa3815584e94463060d5cffe
devlash <vikash_1901me68@iitp.ac.in>
2022-12-20 23:30:54 +0530

    Comment section added

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import './Home.css';
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 export default function Home() {
   const navigate = useNavigate();
   const [data, setData] = useState([]);
+  const [comment, setComment] = useState('');
+  const [show, setShow] = useState(false);
+  const [item, setItem] = useState([]);
+
+  //toast functions
+  // const notifyA = (err) => toast.error(err);
+  const notifyB = (msg) => toast.success(msg);
 
   useEffect(() => {
     const token = localStorage.getItem('jwt');
@@ -21,7 +29,17 @@ export default function Home() {
       .then((res) => res.json())
       .then((result) => setData(result))
       .catch((err) => console.log(err));
-  },[]);
+  }, []);
+
+  // to show and hide comments
+  const toggleComment = (posts) => {
+    if (show) {
+      setShow(false);
+    } else {
+      setShow(true);
+      setItem(posts);
+    }
+  };
 
   const likePost = (id) => {
     fetch('http://localhost:5000/like', {
@@ -69,6 +87,35 @@ export default function Home() {
           }
         });
         setData(newData);
+        console.log(result);
+      });
+  };
+
+  //function to make comment
+  const makeComment = (text, id) => {
+    fetch('http://localhost:5000/comment', {
+      method: 'put',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + localStorage.getItem('jwt'),
+      },
+      body: JSON.stringify({
+        text: text,
+        postId: id,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        const newData = data.map((posts) => {
+          if (posts._id === result._id) {
+            return result;
+          } else {
+            return posts;
+          }
+        });
+        setData(newData);
+        setComment('');
+        notifyB('Comment posted');
         console.log(result);
       });
   };
@@ -120,17 +167,124 @@ export default function Home() {
 
               <p>{posts.likes.length} likes</p>
               <p>{posts.body}</p>
+              <p
+                style={{ fontWeight: 'bold', cursor: 'pointer' }}
+                onClick={() => {
+                  toggleComment(posts);
+                }}
+              >
+                View all comments
+              </p>
             </div>
 
             {/* add comment */}
             <div className="add-comment">
               <span className="material-symbols-outlined">mood</span>
-              <input type="text" placeholder="Add a comment" name="" id="" />
-              <button className="comment">Post</button>
+              <input
+                type="text"
+                placeholder="Add a comment"
+                value={comment}
+                onChange={(e) => {
+                  setComment(e.target.value);
+                }}
+              />
+              <button
+                className="comment"
+                onClick={() => {
+                  makeComment(comment, posts._id);
+                }}
+              >
+                Post
+              </button>
             </div>
           </div>
         );
       })}
+
+      {/* show comments */}
+      {show && (
+        <div className="showComment">
+          <div className="container">
+            <div className="postPic">
+              <img src={item.photo} alt="" />
+            </div>
+            <div className="details">
+              {/* card-header */}
+              <div
+                className="card-header"
+                style={{ borderBottom: '1px solid #00000029' }}
+              >
+                <div className="card-pic">
+                  <img
+                    src="https://cdn.pixabay.com/photo/2021/12/19/15/32/mug-6881412__340.jpg"
+                    alt=""
+                  />
+                </div>
+                <h5>{item.postedBy.name}</h5>
+              </div>
+
+              {/* comment section */}
+              <div
+                className="comment-section"
+                style={{ borderBottom: '1px solid #00000029' }}
+              >
+                {item.comments.map((comment) => {
+                  return (
+                    <p className="comm">
+                      <span
+                        className="commenter"
+                        style={{ fontWeight: 'bolder' }}
+                      >
+                        {comment.postedBy.name}{' '}
+                      </span>
+                      <span className="commentText">{comment.comment}</span>
+                    </p>
+                  );
+                })}
+              </div>
+
+              {/* card-content */}
+              <div className="card-content">
+                <p> {item.likes.length} likes</p>
+                <p>{item.body}</p>
+              </div>
+
+              {/* add comment */}
+              <div className="add-comment">
+                <span className="material-symbols-outlined">mood</span>
+                <input
+                  type="text"
+                  placeholder="Add a comment"
+                  value={comment}
+                  onChange={(e) => {
+                    setComment(e.target.value);
+                  }}
+                />
+                <button
+                  className="comment"
+                  onClick={() => {
+                    makeComment(comment, item._id);
+                    toggleComment();
+                  }}
+                >
+                  Post
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div
+            className="close-comment"
+            onClick={() => {
+              toggleComment();
+            }}
+          >
+            <span className="material-symbols-outlined material-symbols-outlined-comment">
+              close
+            </span>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
